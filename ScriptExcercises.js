f34bebe32484bcf31ce8363a60f91634c3b6d9b7
Camilo Bonilla Hernandez <camilobonillah@gmail.com>
2019-09-19 10:26:54 -0500

    update

---
+++ ScriptExcercises.js
@@ -0,0 +1,6 @@
+function funcionE1(nombreIn, edadIn){
+    const nombre = nombreIn;
+    const edad = edadIn;
+    const sentence = `Hola ${name} Tienes ${age} años y el año que viene tendras ${age + 1} años.'`;
+    console.log(sentence);
+}
